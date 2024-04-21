32acf6e5d1f42560b68e9d8bb15d7dd4df483392
Jonas Marien <marien.jonas@hotmail.com>
2024-04-21 17:28:39 +0200

    Remove backend directory

--- backend/services/AssistantFactory.cs
+++
@@ -1,9 +0,0 @@
-public interface IAssistantFactory {
-    public Task<IAssistant> Create();
-}
-
-public class AssistantFactory : IAssistantFactory {
-    public Task<IAssistant> Create() {
-        throw new NotImplementedException();
-    }
-}
